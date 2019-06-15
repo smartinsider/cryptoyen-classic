86cf2f7a9420213dc278a800937a199a2abbcdeb
SmartInsider <smartyinsider@gmail.com>
2019-06-15 14:54:16 +0300

    miner Update Test006

--- src/miner.cpp
+++ src/miner.cpp
@@ -127,9 +127,7 @@ CBlockTemplate* CreateNewBlock(const CScript& scriptPubKeyIn, CWallet* pwallet, 
     txNew.vin.resize(1);
     txNew.vin[0].prevout.SetNull();
     txNew.vout.resize(1);
-    //txNew.vout[0].SetEmpty();
-
-    LogPrintf("ZERO HERE = 001\n");
+    txNew.vout[0].SetEmpty();
 
     LogPrintf("CreateNewBlock() : chainActive.Height() = %s \n", chainActive.Height());
     if (chainActive.Height() >= Params().LAST_POW_BLOCK()) {
