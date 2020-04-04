5eeb24ef5476242c874f540a186e71c7e3070b46
coin-universe <coin-universe@users.noreply.github.com>
2020-04-04 17:43:05 +0300

    Update chackpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -66,11 +66,11 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
       (80000, uint256("0x3623f034aa19b5b36d74a7a6a4262505f27913a102a72236929b5a9d5bc6d098"))
       (80008, uint256("0x92c7a8d01cda2f3dccd85feb192021526280fdb6977bccfd4526f082011538e1"))
       (80213, uint256("0xfe92c41cb3e872d492fdff7fe50d4db15699880ec01c67b5bee86150e5296619"))
-      (175448, uint256("0x297dfc81973f3d0c53f9ba1d55da3bbfb3ad4c00ed3843d8cdf2d1dcfa806cbc"))      
+      (175448, uint256("0x14ace011f7ee65123731ada687df4e2213015e6bd7833dec14ebc98d005e0c9b"))      
     ;
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1585844424, // * UNIX timestamp of last checkpoint block
+    1585829879, // * UNIX timestamp of last checkpoint block
     361027,     // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
