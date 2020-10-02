1ca22dac7cb6dc4675f67ef627c0bc593e56bfc0
coin-universe <reyner.info@gmail.com>
2020-10-02 17:58:40 +0200

    fix connection

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -67,12 +67,13 @@ static Checkpoints::MapCheckpoints mapCheckpoints =
       (80008, uint256("0x92c7a8d01cda2f3dccd85feb192021526280fdb6977bccfd4526f082011538e1"))
       (80213, uint256("0xfe92c41cb3e872d492fdff7fe50d4db15699880ec01c67b5bee86150e5296619"))
       (175448, uint256("0x14ace011f7ee65123731ada687df4e2213015e6bd7833dec14ebc98d005e0c9b"))
-      (470001, uint256("0x44071d7fcf7cf160665e9ef5c2c80477e38e2fdfda5845211775199ecb75e1a2"))
+	  (470001, uint256("0xa643a253d04b02980f2eb25ac392b32043801119ab9cb8678b46f60f2bfe14dc"))
+	  (470060, uint256("0xe81e5c9673972c52eadfc48db5aa3a1952aaf9da6c7b1f2c61336364a2757c9a"))
     ;
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1601458208, // * UNIX timestamp of last checkpoint block
-    971677,     // * total number of transactions between genesis and last checkpoint
+    1601650823, // * UNIX timestamp of last checkpoint block
+    971795,     // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
 };
@@ -158,8 +159,14 @@ public:
         hashGenesisBlock = genesis.GetHash();
         assert(hashGenesisBlock == uint256("0000082cafc7c356e4ec3d9c25b00c7b463edc466fa5c8fcfdbcba59c34f2c72"));
         assert(genesis.hashMerkleRoot == uint256("3bbfa4b9fe24355127acdd9ebe919cde06fed041f7f0128623c4e61e743bf338"));
+		
+		//STABILE IP
+        vSeeds.push_back(CDNSSeedData("ns1.cryptoyen.icu", "ns1.divinelife.club"));
+        vSeeds.push_back(CDNSSeedData("ns2.cryptoyen.icu", "ns2.divinelife.club"));
+        vSeeds.push_back(CDNSSeedData("ns3.cryptoyen.icu", "ns3.divinelife.club"));
+        vSeeds.push_back(CDNSSeedData("ns4.cryptoyen.icu", "ns4.divinelife.club"));
 
-	//DNS SEEDS
+	    //DNS SEEDS
         vSeeds.push_back(CDNSSeedData("divinelife.club", "seeder1.divinelife.club"));
         vSeeds.push_back(CDNSSeedData("divinelife.club", "seeder2.divinelife.club"));
 
